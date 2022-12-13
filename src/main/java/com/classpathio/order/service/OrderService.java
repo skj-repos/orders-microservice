13a48503602ff26aa42a6865c503cf95c2bc7da6
pradeep <pradeep.kumar44@gmail.com>
2022-12-13 12:27:05 +0530

    committing the changes in the order microservice

--- src/main/java/com/classpathio/order/service/OrderService.java
+++ src/main/java/com/classpathio/order/service/OrderService.java
@@ -3,20 +3,32 @@ package com.classpathio.order.service;
 import java.util.Set;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.classpathio.order.model.Order;
 import com.classpathio.order.repository.OrderJpaRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 	
 	private final OrderJpaRepository orderRepository;
+	private final WebClient webClient;
 	
 	public Order saveOrder(Order order) {
-		return this.orderRepository.save(order);
+		Order savedOrder = this.orderRepository.save(order);
+		//make the rest call and update the inventory
+		long orderCount = this.webClient
+								.post()
+								.uri("/api/inventory")
+								.exchangeToMono(res -> res.bodyToMono(Long.class))
+								.block();
+		log.info("Response from inventory microservice :: {}", orderCount);
+		return savedOrder;
 	}
 	
 	public Set<Order> fetchOrders(){
