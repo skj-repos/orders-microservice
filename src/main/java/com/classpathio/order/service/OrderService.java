25ac97cb852a76abb80e84692ccac86cc8969078
pradeep <pradeep.kumar44@gmail.com>
2022-12-14 14:47:59 +0530

    committing the changes

--- src/main/java/com/classpathio/order/service/OrderService.java
+++ src/main/java/com/classpathio/order/service/OrderService.java
@@ -1,13 +1,21 @@
 package com.classpathio.order.service;
 
+import java.time.LocalDateTime;
 import java.util.Set;
+
+import javax.transaction.Transactional;
 
 import org.springframework.boot.availability.AvailabilityChangeEvent;
 import org.springframework.boot.availability.ReadinessState;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.classpathio.order.event.OrderEvent;
+import com.classpathio.order.event.OrderStatus;
 import com.classpathio.order.model.Order;
 import com.classpathio.order.repository.OrderJpaRepository;
 
@@ -24,19 +32,26 @@ public class OrderService {
 	private final OrderJpaRepository orderRepository;
 	private final WebClient webClient;
 	private final ApplicationEventPublisher applicationEvent;
+	private final StreamBridge streamBridge;
 	
 	@CircuitBreaker(name="inventoryservice", fallbackMethod = "fallback")
 	@Retry(name="retryConfig")
+	@Transactional
 	public Order saveOrder(Order order) {
 		Order savedOrder = this.orderRepository.save(order);
 		log.info("Calling the inventory microservice :: ");
 		//make the rest call and update the inventory
-		long orderCount = this.webClient
-								.post()
-								.uri("/api/inventory")
-								.exchangeToMono(res -> res.bodyToMono(Long.class))
-								.block();
-		log.info("Response from inventory microservice :: {}", orderCount);
+		/*
+		 * long orderCount = this.webClient .post() .uri("/api/inventory")
+		 * .exchangeToMono(res -> res.bodyToMono(Long.class)) .block();
+		 */
+		//create an order event and publish to the broker
+		OrderEvent orderEvent = new OrderEvent(savedOrder, OrderStatus.ORDER_ACCEPTED, LocalDateTime.now());
+		Message<OrderEvent> message = MessageBuilder.withPayload(orderEvent).build();
+		
+		//publish this message to the broker
+		this.streamBridge.send("producer-out-0", message);
+		//log.info("Response from inventory microservice :: {}", orderCount);
 		return savedOrder;
 	}
 	
